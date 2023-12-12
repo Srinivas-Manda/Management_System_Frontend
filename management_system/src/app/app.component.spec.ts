fabe90f53428d2243caf2d76ea36312d79544235
manda <srinivas911032@gmail.com>
2023-12-12 12:10:30 +0530

    added subpost and component functionality

--- management_system/src/app/app.component.spec.ts
+++ management_system/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'management_system' title`, () => {
+  it(`should have as title 'angular-iiitbcms'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('management_system');
+    expect(app.title).toEqual('angular-iiitbcms');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, management_system');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-iiitbcms app is running!');
   });
 });
